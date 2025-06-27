69b598fb994e49772800e6486d72bcc543dddbb1
zainmorshed <141587497+zainmorshed@users.noreply.github.com>
2025-06-27 15:37:38 -0500

    Update JobApplicationController.java

--- src/main/java/com/zainmorshed/jobtracker/controller/JobApplicationController.java
+++ src/main/java/com/zainmorshed/jobtracker/controller/JobApplicationController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/jobs")
+@CrossOrigin(origins = "http://localhost:5173")
 public class JobApplicationController {
 
     private final JobApplicationService jobApplicationService;
