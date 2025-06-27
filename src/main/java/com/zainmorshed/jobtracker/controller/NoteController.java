6ae24056984352e5499476f13d230cabc0c4455a
zainmorshed <141587497+zainmorshed@users.noreply.github.com>
2025-06-27 15:41:55 -0500

    Update NoteController.java

--- src/main/java/com/zainmorshed/jobtracker/controller/NoteController.java
+++ src/main/java/com/zainmorshed/jobtracker/controller/NoteController.java
@@ -1,6 +1,7 @@
 package com.zainmorshed.jobtracker.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
