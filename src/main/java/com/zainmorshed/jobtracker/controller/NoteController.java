08f7113f4b7fad50d7294362b791eff6e6401a69
zainmorshed <141587497+zainmorshed@users.noreply.github.com>
2025-06-27 15:42:14 -0500

    Update NoteController.java

--- src/main/java/com/zainmorshed/jobtracker/controller/NoteController.java
+++ src/main/java/com/zainmorshed/jobtracker/controller/NoteController.java
@@ -1,55 +1,54 @@
 package com.zainmorshed.jobtracker.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 import java.util.List;
 
-import com.zainmorshed.jobtracker.model.Note;
-import com.zainmorshed.jobtracker.service.NoteService;
+import com.zainmorshed.jobtracker.model.User;
+import com.zainmorshed.jobtracker.service.UserService;
 
-@RestController 
-@RequestMapping("/api/notes")
+@RestController
+@RequestMapping("/api/users")
 @CrossOrigin(origins = "http://localhost:5173")
-public class NoteController {
-    
-    private final NoteService noteService;
+public class UserController {
 
+    private final UserService userService;
     @Autowired
-    public NoteController(NoteService noteService) {
-        this.noteService = noteService;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping
-    public List<Note> getAllNotes() {
-        return noteService.getAllNotes();
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
 
     @GetMapping("/{id}")
-    public Note getNoteById(@PathVariable Long id) {
-        return noteService.getNoteById(id);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteNote(@PathVariable Long id) {
-        noteService.deleteNote(id);
-    }
-
-    @PutMapping("/{id}")
-    public Note updateNote(@PathVariable Long id, @RequestBody Note note){
-        return noteService.updateNote(id, note);
+    public User getUserByID(@PathVariable Long id){
+        return userService.getUserByID(id);
     }
 
     @PostMapping
-    public Note saveNote(@RequestBody Note note) {
-        return noteService.saveNote(note);
+    public User saveUser(@RequestBody User user) {
+        return userService.saveUser(user);
     }
 
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+    }
 }
