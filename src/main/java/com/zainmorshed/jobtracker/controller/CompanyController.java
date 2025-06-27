8ad8bff67ad6a62502d6ae8acb67a7d6929a6b4a
zainmorshed <141587497+zainmorshed@users.noreply.github.com>
2025-06-27 15:41:02 -0500

    Update CompanyController.java

--- src/main/java/com/zainmorshed/jobtracker/controller/CompanyController.java
+++ src/main/java/com/zainmorshed/jobtracker/controller/CompanyController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
